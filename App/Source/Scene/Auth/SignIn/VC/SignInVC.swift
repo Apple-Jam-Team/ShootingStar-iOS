13f4236132db0cfeed0d815bf72a2042238efef8
hsj._.06 <gtw030488@gmail.com>
2022-12-17 19:10:04 +0900

    로그인 VC

--- App/Source/Scene/Auth/SignIn/VC/SignInVC.swift
+++ App/Source/Scene/Auth/SignIn/VC/SignInVC.swift
@@ -1,9 +1,35 @@
-//
-//  SignInVC.swift
-//  ShootingStar
-//
-//  Created by 홍승재 on 2022/12/17.
-//  Copyright © 2022 com.asdf. All rights reserved.
-//
+import UIKit
+import Firebase
+import Then
+import SnapKit
 
-import Foundation
+class SignInVC: BaseVC<BaseViewModel> {
+    private var emailTF = UITextField().then {
+        $0.placeholder = "이메일을 입력하세요"
+    }
+    private var passwordTF = UITextField().then {
+        $0.placeholder = "비밀번호를 입력하세요"
+        $0.isSecureTextEntry = true
+    }
+    override func configureVC() {
+//        view.backgroundColor = .blue
+    }
+    override func addView() {
+        [
+            emailTF,
+            passwordTF
+        ].forEach {
+            view.addSubview($0)
+        }
+    }
+    override func setLayout() {
+        emailTF.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview()
+        }
+        passwordTF.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(emailTF.snp.bottom).inset(-30)
+        }
+    }
+}
